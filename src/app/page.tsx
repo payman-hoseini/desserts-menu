08554001f75c09cb0e045e2286ba914825dafc9c
Payman <payman.hoseini7@gmail.com>
2024-08-29 23:15:54 +0430

    Studing  How to Load Data from a File in Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { promises as fs } from 'fs';
 
-
-export default function Home() {
+export default async function Home() {
+  
+  const file = await fs.readFile(process.cwd() + '/src/app/data.json' , 'utf8')
+  const data = JSON.parse(file)
   return (
    <>
-       
+      <p>{data[0].name}</p>
    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors : {
+        'Red' : 'hsl(14, 86%, 42%)',
+        'Green' : 'hsl(159, 69%, 38%)',
+        'Rose' : {
+          50 : 'hsl(20, 50%, 98%)',
+          100 : 'hsl(13, 31%, 94%)',
+          300 : 'hsl(14, 25%, 72%)',
+          400 : 'hsl(7, 20%, 60%)',
+          500 : 'hsl(12, 20%, 44%)',
+          900 : 'hsl(14, 65%, 9%)',
+        }
+      },
+      fontFamily : {
+        RedHat : ['RedHat']
+      }
     },
   },
   plugins: [],
