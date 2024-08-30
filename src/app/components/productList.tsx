00158b54ec53bfc1d8e5e1c4482d46b32e3b2b82
Payman <payman.hoseini7@gmail.com>
2024-08-30 11:26:43 +0430

    put assets folder in public directory

--- src/app/components/productList.tsx
+++ src/app/components/productList.tsx
@@ -1,10 +1,21 @@
-import { Product } from "../Contract";
 
+import { Product } from "../Contract";
 
 export default function ProductList({product} : {product : Product}){
     return (
         <>
-            
+        {/* koli */}
+            <div> 
+                {/* mahsol */}
+                <div>
+                    <div>
+                        <img src={product.image.desktop} alt={product.name}/>
+                        <button>
+                            Add
+                        </button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
