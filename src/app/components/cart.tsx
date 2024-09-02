f86723f4c02178ce504fee51887a7529ef1d2a93
Payman <payman.hoseini7@gmail.com>
2024-09-02 11:44:49 +0430

    Handle show and close Modal

--- src/app/components/cart.tsx
+++ src/app/components/cart.tsx
@@ -2,7 +2,7 @@
 import { useSelector } from "react-redux"
 import { cancelProduct, selectList, selectTotal } from "./store/cartSlice"
 import { useAppDispatch } from "./store/hooks"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 
@@ -18,7 +18,24 @@ export default function Cart (){
         return perv + current
     },0)
 
-   
+    // handle Modal Close //
+    useEffect(() => {
+        if(showModal){
+            document.addEventListener('mousedown' , handleClickOutside)
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    },[showModal])
+
+    const modalRef = useRef(null);
+    const handleClickOutside = (event : any) => {
+        if (modalRef.current == event.target) {
+            setShowModal(false)
+        }
+        
+    }
     
     return (
         <>
@@ -79,7 +96,7 @@ export default function Cart (){
             {/* Modal */}
             {
                 showModal && 
-                <div className="fixed z-10 left-0 top-0 w-full h-full bg-black bg-opacity-50 grid place-content-center">
+                <div ref={modalRef} className="fixed z-10 left-0 top-0 w-full h-full bg-black bg-opacity-50 grid place-content-center">
                     <div className="bg-white py-8 px-8 rounded-lg w-[25vw]">
                         <div>
                             <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M21 32.121L13.5 24.6195L15.6195 22.5L21 27.879L32.3775 16.5L34.5 18.6225L21 32.121Z" fill="#1EA575"/><path d="M24 3C19.8466 3 15.7865 4.23163 12.333 6.53914C8.8796 8.84665 6.18798 12.1264 4.59854 15.9636C3.0091 19.8009 2.59323 24.0233 3.40352 28.0969C4.21381 32.1705 6.21386 35.9123 9.15077 38.8492C12.0877 41.7861 15.8295 43.7862 19.9031 44.5965C23.9767 45.4068 28.1991 44.9909 32.0364 43.4015C35.8736 41.812 39.1534 39.1204 41.4609 35.667C43.7684 32.2135 45 28.1534 45 24C45 18.4305 42.7875 13.089 38.8493 9.15076C34.911 5.21249 29.5696 3 24 3ZM24 42C20.4399 42 16.9598 40.9443 13.9997 38.9665C11.0397 36.9886 8.73256 34.1774 7.37018 30.8883C6.0078 27.5992 5.65134 23.98 6.34587 20.4884C7.04041 16.9967 8.75474 13.7894 11.2721 11.2721C13.7894 8.75473 16.9967 7.0404 20.4884 6.34587C23.98 5.65133 27.5992 6.00779 30.8883 7.37017C34.1774 8.73255 36.9886 11.0397 38.9665 13.9997C40.9443 16.9598 42 20.4399 42 24C42 28.7739 40.1036 33.3523 36.7279 36.7279C33.3523 40.1036 28.7739 42 24 42Z" fill="#1EA575"/></svg>
@@ -91,10 +108,12 @@ export default function Cart (){
                                 {
                                     list.map(product => (product.count > 0) ?
                                     <>
-                                        <div className="flex items-center justify-between border-b border-b-Rose-100 pb-3">
-                                                <div className="flex items-center">
-                                                    <img className="rounded-lg w-[23%]" src={product.product.image.thumbnail} alt={product.product.name} />
-                                                    <div className="ml-3">
+                                        <div className="flex items-center justify-between border-b border-b-Rose-100 py-3">
+                                                <div className="flex items-center justify-start">
+                                                    <div>
+                                                        <img className="rounded-lg w-[60%]" src={product.product.image.thumbnail} alt={product.product.name} />
+                                                    </div>
+                                                    <div className="-ml-3 basis-[90%]">
                                                         <p className="font-semibold text-sm text-Rose-900">{product.product.name}</p>
                                                         <div>
                                                             <span className="text-Red font-semibold text-sm">{product.count}x</span>
